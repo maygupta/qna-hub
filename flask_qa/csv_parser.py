5a1ea7fc5d644df65c92c6f2b790d5b5f9f3177a
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2021-04-02 20:32:58 -0700

    fix upload issue

--- flask_qa/csv_parser.py
+++ flask_qa/csv_parser.py
@@ -15,42 +15,41 @@ class CSVParser:
       csv_reader = csv.DictReader(csv_file)
       line_count = 0
       for row in csv_reader:
-         if line_count == 0:
-             line_count += 1
-         else:
-          user = User.query.filter(User.name.ilike(row['question_author_name'])) \
-                      .first()
-          is_ques_duplicate = len(Question.query.filter(
-            func.lower(Question.question) == row['question'].lower())
-          .all()) > 0
-          
-          if is_ques_duplicate:
-            raise BadRequest("Question %s already exists" % row['question'])
+        user = User.query.filter(User.name.ilike(row['question_author_name'])) \
+                    .first()
+        is_ques_duplicate = len(Question.query.filter(
+          func.lower(Question.question) == row['question'].lower())
+        .all()) > 0
 
-          if user:    
-            question = Question(question=row['question'], asked_by_id=user.id)
-          else:
-            User()
-            question = Question(question=row['question'], asked_by_id=current_user.id)
-          db.session.add(question)
+        print is_ques_duplicate, row
+        
+        if is_ques_duplicate:
+          raise BadRequest("Question %s already exists" % row['question'])
+
+        if user:    
+          question = Question(question=row['question'], asked_by_id=user.id)
+        else:
+          User()
+          question = Question(question=row['question'], asked_by_id=current_user.id)
+        db.session.add(question)
+        db.session.commit()
+
+        if row['answer'] != '':
+          answer = Answer(text=row['answer'], question_id=question.id, added_by=current_user.id)
+          db.session.add(answer)
           db.session.commit()
 
-          if row['answer'] != '':
-            answer = Answer(text=row['answer'], question_id=question.id, added_by=current_user.id)
-            db.session.add(answer)
-            db.session.commit()
-
-          if row['tags'] != '':
-            for tag_text in row['tags'].split(","):
-              tag = Tag.query.filter(Tag.name.ilike(tag_text.lower())).first()
-              if not tag:
-                tag = Tag(name=tag_text)
-                db.session.add(tag)
-                db.session.commit()
-
-              tag_map = TagQuestionMap(tag_id=tag.id, question_id=question.id)
-              db.session.add(tag_map)
+        if row['tags'] != '':
+          for tag_text in row['tags'].split(","):
+            tag = Tag.query.filter(Tag.name.ilike(tag_text.lower())).first()
+            if not tag:
+              tag = Tag(name=tag_text)
+              db.session.add(tag)
               db.session.commit()
+
+            tag_map = TagQuestionMap(tag_id=tag.id, question_id=question.id)
+            db.session.add(tag_map)
+            db.session.commit()
 
 
 
